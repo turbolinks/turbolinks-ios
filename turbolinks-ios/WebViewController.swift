2fdc7b62cf2d30373f9393401e668ce18e016787
Jeffrey Hardy <packagethief@gmail.com>
2015-06-30 12:16:21 -0400

    Use a shared web view

--- turbolinks-ios/WebViewController.swift
+++ turbolinks-ios/WebViewController.swift
@@ -1,52 +1,96 @@
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController, WKScriptMessageHandler {
+protocol WebViewControllerNavigationDelegate {
+    func pageWillChange(URL: NSURL!)
+}
+
+class WebViewController: UIViewController, WebViewControllerNavigationDelegate {
+    class ScriptHandler: NSObject, WKScriptMessageHandler {
+        static let instance: ScriptHandler = {
+            return ScriptHandler()
+        }()
+
+        var delegate: WebViewControllerNavigationDelegate?
+
+        func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+            if let body = message.body as? [String: AnyObject] {
+                var name = body["name"] as? String
+                var data = body["data"] as? String
+
+                switch name! {
+                case "page:before-change":
+                    self.delegate?.pageWillChange(NSURL(string: data!))
+                case "log":
+                    println(data)
+                default:
+                    println("Unhandled message: \(name): \(data)")
+                }
+            }
+        }
+    }
+
     var URL = NSURL(string: "http://turbolinks.dev/")
 
-    lazy var webView: WKWebView = {
+    static let sharedWebView: WKWebView = {
         let configuration = WKWebViewConfiguration()
         configuration.userContentController = WKUserContentController()
 
         let webView = WKWebView(frame: CGRectZero, configuration: configuration)
         webView.setTranslatesAutoresizingMaskIntoConstraints(false)
         webView.scrollView.decelerationRate = UIScrollViewDecelerationRateNormal
-        webView.configuration.userContentController.addScriptMessageHandler(self, name: "bridgeMessage")
 
         let appSource = NSString(contentsOfURL: NSBundle.mainBundle().URLForResource("app", withExtension: "js")!, encoding: NSUTF8StringEncoding, error: nil)!
         webView.configuration.userContentController.addUserScript(WKUserScript(source: appSource as! String, injectionTime: WKUserScriptInjectionTime.AtDocumentStart, forMainFrameOnly: true))
+        webView.configuration.userContentController.addScriptMessageHandler(ScriptHandler.instance, name: "bridgeMessage")
 
         return webView
     }()
 
-    init(URL: NSURL) {
-        super.init(nibName: nil, bundle: nil)
-        self.URL = URL
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-
-    override func loadView() {
-        self.view = webView
-    }
+    lazy var webView: WKWebView = {
+        return WebViewController.sharedWebView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.webView.loadRequest(NSURLRequest(URL: self.URL!))
+        edgesForExtendedLayout = .None
+        automaticallyAdjustsScrollViewInsets = false
     }
 
-    // MARK: - WKScriptMessageHandler
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareScriptHandler()
+        insertWebView()
+        loadRequest()
+    }
 
-    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
-        println("\(message.name): \(message.body)")
+    private func prepareScriptHandler() {
+        ScriptHandler.instance.delegate = self
+    }
 
-        if let body = message.body as? [String: AnyObject] {
-            if body["name"] as? String == "page:before-change" {
-                let webViewController = WebViewController(URL: NSURL(string: body["data"] as! String)!)
-                self.navigationController?.pushViewController(webViewController, animated: true)
-            }
+    private func insertWebView() {
+        view.addSubview(webView)
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|", options: nil, metrics: nil, views: [ "view": webView ]))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[view]|", options: nil, metrics: nil, views: [ "view": webView ]))
+    }
+
+    private func loadRequest() {
+        if webView.URL == nil {
+            webView.loadRequest(NSURLRequest(URL: URL!))
+        } else {
+            loadRequestWithTurbolinks()
         }
+    }
+
+    private func loadRequestWithTurbolinks() {
+        webView.evaluateJavaScript("Turbolinks.visit('\(URL!.absoluteString!)')", completionHandler: nil)
+    }
+
+    // MARK - WebViewControllerNavigationDelegate
+
+    func pageWillChange(URL: NSURL!) {
+        let webViewController = WebViewController()
+        webViewController.URL = URL
+        navigationController?.pushViewController(webViewController, animated: true)
     }
 }
