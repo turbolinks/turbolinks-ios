297c75dc98b7d9362e8681827c388430cd7c3f74
Sam Stephenson <sam@37signals.com>
2015-07-29 12:35:49 -0500

    Turn on some more native options

--- TurbolinksDemo/TurbolinksDemo/TurbolinksDemo.js
+++ TurbolinksDemo/TurbolinksDemo/TurbolinksDemo.js
@@ -1,1 +1,1 @@
-document.documentElement.dataset.bridgeConfiguration = "ios"
+document.documentElement.dataset.bridgeConfiguration = "ios disable-breadcrumbs disable-headers disable-folders disable-follow disable-options-menu"
