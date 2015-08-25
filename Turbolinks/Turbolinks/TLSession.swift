5d276f991fba8c457612222b35768557bf41bd85
Jeffrey Hardy <packagethief@gmail.com>
2015-08-24 20:09:50 -0400

    Visitable deactivation happens automatically when a different visitable is activated

--- Turbolinks/Turbolinks/TLSession.swift
+++ Turbolinks/Turbolinks/TLSession.swift
@@ -194,7 +194,6 @@ public class TLSession: NSObject, TLWebViewDelegate, TLVisitDelegate, TLVisitabl
     }
     
     public func visitableViewDidDisappear(visitable: TLVisitable) {
-        deactivateVisitable(visitable)
     }
 
     public func visitableViewDidAppear(visitable: TLVisitable) {
