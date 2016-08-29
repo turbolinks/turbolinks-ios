54c997d075feed2c753d460da28f288dd1a624b5
Dylan Ginsburg <dylan@basecamp.com>
2016-08-29 15:13:44 -0400

    Updated for swift 2.3 and Xcode 8

--- Turbolinks/WebView.swift
+++ Turbolinks/WebView.swift
@@ -37,6 +37,10 @@ class WebView: WKWebView {
         translatesAutoresizingMaskIntoConstraints = false
         scrollView.decelerationRate = UIScrollViewDecelerationRateNormal
     }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
     func visitLocation(location: NSURL, withAction action: Action, restorationIdentifier: String?) {
         callJavaScriptFunction("webView.visitLocationWithActionAndRestorationIdentifier", withArguments: [location.absoluteString, action.rawValue, restorationIdentifier])
