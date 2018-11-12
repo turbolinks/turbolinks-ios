9804661174ef896298d50f2daa34154c0758e9a9
Kevin Clark <kevinclark311@gmail.com>
2018-11-12 17:52:45 -0500

    Change code to category
    * Add method to extract useful code for all error formats

--- Turbolinks/Error.swift
+++ Turbolinks/Error.swift
@@ -2,21 +2,42 @@ import Foundation
 
 public let ErrorDomain = "com.basecamp.Turbolinks"
 
-public enum ErrorCode: Int {
+public enum ErrorCategory: Int {
     case httpFailure
     case networkFailure
+    case unknown
 }
 
 extension NSError {
-    convenience init(code: ErrorCode, localizedDescription: String) {
+    open func getCode() -> Int {
+        switch getCategory() {
+        case .httpFailure:
+            return self.userInfo["statusCode"] as! Int
+        case .networkFailure:
+            return getDetailedCode()
+        default:
+            return self.code
+        }
+    }
+    
+    func getCategory() -> ErrorCategory {
+        return ErrorCategory(rawValue: self.code) ?? .unknown
+    }
+    
+    func getDetailedCode() -> Int {
+        guard let wrappedError = self.userInfo["error"] as? NSError else { return -1 }
+        return wrappedError.code
+    }
+    
+    convenience init(code: ErrorCategory, localizedDescription: String) {
         self.init(domain: ErrorDomain, code: code.rawValue, userInfo: [NSLocalizedDescriptionKey: localizedDescription])
     }
    
-    convenience init(code: ErrorCode, statusCode: Int) {
+    convenience init(code: ErrorCategory, statusCode: Int) {
         self.init(domain: ErrorDomain, code: code.rawValue, userInfo: ["statusCode": statusCode, NSLocalizedDescriptionKey: "HTTP Error: \(statusCode)"])
     }
 
-    convenience init(code: ErrorCode, error: NSError) {
+    convenience init(code: ErrorCategory, error: NSError) {
         self.init(domain: ErrorDomain, code: code.rawValue, userInfo: ["error": error, NSLocalizedDescriptionKey: error.localizedDescription])
     }
 }
